492294f5a99267966b5537f6d1520237ffeee51d
Viiperrino <67992281+Viiperrino@users.noreply.github.com>
2023-09-27 23:05:01 -0700

    print direction
    print direction

---
+++ cmake-build-debug/CMakeFiles/nPuzzle.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nPuzzle.dir/main.cpp.o"
+  "CMakeFiles/nPuzzle.dir/main.cpp.o.d"
+  "nPuzzle"
+  "nPuzzle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nPuzzle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
